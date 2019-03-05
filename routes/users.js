570d5530a45387cd1b7fd6188c7f2d7e050c4ab4
Warren <warrenisaac84@gmail.com>
2019-03-04 19:46:37 -0600

    Refactoring to use async await.

--- routes/users.js
+++ routes/users.js
@@ -5,51 +5,36 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
-// Initialize the router
 const router = express.Router();
-// Imports from the User model
-const { User, validate } = require('../models/user');
+const {
+  User,
+  validate
+} = require('../models/user');
 
 // Stores a new user in the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // Validate new user request body against userSchema
-  const { error } = validate(req.body);
+  const {
+    error
+  } = validate(req.body);
 
   // Request does not match userSchema
-  if (error) {
-    return res.status(400).json({message: error.details[0].message});
-  }
-
-  let user;
+  if (error) return res.status(400).json({
+    message: error.details[0].message
+  });
 
   // Verify user does not already exist by checking email
-  user = User.findOne({email: req.body.email})
-    .then((account) => {
-      // Return JWT token for registered user
-      if (account) {
-        // Get userName and email values from User
-        const userDetails = _.pick(user, ['userName', 'email']);
+  let user = await User.findOne({email: req.body.email})
+  try {
+    if (user && user.email) {
+      // Account already exists
+      return res.status(400).json({message: 'Account already exists'});
+    }
 
-        // Generate JWT with userName and email
-        const token = jwt.sign(userDetails, process.env.JWT_KEY);
+  } catch (ex) {
+    return res.status(500).json({message: ex});
+  }
 
-        // Send response to the client
-        return res.send({
-          code: 204,
-          message: 'Existing user login success.',
-          token: token
-        });
-      }
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        message: 'Internal server error.',
-        error: err
-      });
-    });
-
-
-  // Create new User from the request body properties
   user = new User({
     _id: new mongoose.Types.ObjectId(),
     userName: req.body.userName,
@@ -58,20 +43,31 @@ router.post('/', (req, res) => {
     favoriteSpot: req.body.favoriteSpot
   });
 
+  await user.save()
+    try {
+      console.log(`Successfully created user ${user.userName}.`);
 
-  // Store user in the database
-  saveUser(user, res);
+      const userProfile = _.pick(user, ['userName', 'email', 'tribe', 'favoriteSpot']);
+      // Generate JWT
+      const token = jwt.sign(userProfile, process.env.JWT_KEY);
+
+      return res.status(201).json({
+        user: userProfile,
+        message: 'Account created success.',
+        token: token
+      });
+
+  } catch (ex) {
+    console.log('Failed to create user: ' + ex);
+    return res.status(500).json({message: ex});
+  }
 });
+
 
 router.post('/profile', (req, res) => {
 
   User.find()
     .then((users) => {
-
-      // Return 404 when user profile doesn't exist
-      // if (users && !users.userName) {
-      //   return res.status(404).json({message: 'User not found.'});
-      // }
 
       let userProfile = users.filter((user) => user.email === req.body.email);
 
@@ -92,19 +88,27 @@ router.post('/profile', (req, res) => {
 router.patch('/', (req, res) => {
 
   // Find user in database by email
-  User.findOne({email: req.body.email})
+  User.findOne({
+      email: req.body.email
+    })
     .then((user) => {
       updateUserProfile(user, req, res);
     })
     .catch((err) => {
-      return res.status(500).json({message: err});
+      return res.status(500).json({
+        message: err
+      });
     });
 });
 
 function updateUserProfile(user, req, res) {
 
   // Push the tribe into the User profile
-  user.update({$push: {tribe: req.body.tribeName}})
+  user.update({
+      $push: {
+        tribe: req.body.tribeName
+      }
+    })
 
     .then((data) => {
       return res.status(201).json({
@@ -114,33 +118,16 @@ function updateUserProfile(user, req, res) {
     })
     .catch((err) => {
       console.log('Unable to update user profile.' + err);
-      return res.status(500).json({message: 'Interal server error occurred.'});
+      return res.status(500).json({
+        message: 'Interal server error occurred.'
+      });
     });
 
 }
 
-
 function saveUser(user, res) {
-  user.save()
-    .then(() => {
-      console.log(`Successfully created user ${user.userName}.`)
-
-      const userDetails = _.pick(user, ['userName', 'email']);
-
-      // Generate JWT
-      const token = jwt.sign(userDetails, process.env.JWT_KEY);
 
 
-      return res.status(201).json({
-        code: 201,
-        message: 'Account created success.',
-        token: token
-      });
-    })
-    .catch((err) => {
-      console.log('Failed to create user: ' + err);
-      return res.status(500).json({message: 'Interal server error occurred.'})
-    });
 }
 
 
